5553ae1448c801d14231f6a61afeecf8942478a6
thquyen11 <thquyen11@hotmail.com>
2020-01-25 11:23:12 +0700

    complete assignment5

--- cev_duprie.py
+++ cev_duprie.py
@@ -17,12 +17,18 @@ T = 3
 test_strikes = np.linspace(80,120,41)
 
 # Call price under CEV
-def C(t, K):
-    kappa = 2*r/(sigma**2*(2-beta)*(np.exp(r*(2-beta)*t)-1)) 
-    x = kappa*S0**(2-beta)*np.exp(r*(2-beta)*t) 
-    y = kappa*K**(2-beta) 
-    zb = 2 + 2/(2-beta)
-    return S0*(1-(ncx2.cdf(2*y,zb,2*x)))-K*(1-(1-ncx2.cdf(2*x,zb-2,2*y)))*np.exp(-r*t)
+def C(t,K):
+    kappa = 2*r/(sigma**2*(1-gamma)*(np.exp(2*r*(1-gamma)*t)-1))
+    x = kappa*S0**(2*(1-gamma))*np.exp(2*r*(1-gamma)*t)
+    y = kappa*K**(2*(1-gamma))
+    z = 2 + 1/(1-gamma)
+    return S0*(1-ncx2.cdf(y,z,x))-K*np.exp(-r*t)*ncx2.cdf(x,z-2,y)
+# def C(t, K):
+#     kappa = 2*r/(sigma**2*(2-beta)*(np.exp(r*(2-beta)*t)-1)) 
+#     x = kappa*S0**(2-beta)*np.exp(r*(2-beta)*t) 
+#     y = kappa*K**(2-beta) 
+#     zb = 2 + 2/(2-beta)
+#     return S0*(1-(ncx2.cdf(2*y,zb,2*x)))-K*(1-(1-ncx2.cdf(2*x,zb-2,2*y)))*np.exp(-r*t)
     
 #Estimating partial derivatives
 delta_t = 0.01
@@ -33,7 +39,7 @@ d2C_dK2 = (C(T,test_strikes+2*delta_K)-2*C(T,test_strikes+delta_K)+C(T,test_stri
 
 #Estimating local volatility - Dupire Equation
 vol_est = np.sqrt(2)/test_strikes*np.sqrt((dC_dT+r*test_strikes*dC_dK)/d2C_dK2)
-z1=C(T,K)/400
+# z1=C(T,K)/400
 
 #Plotting closed-form and Dupire equation
 plt.subplot(211)
@@ -41,8 +47,8 @@ plt.plot(test_strikes, sigma*test_strikes**(gamma-1), label="Closed-form")
 plt.grid(True)
 plt.legend()
 plt.subplot(212)
-plt.plot(test_strikes, z1, lw=3, label="Dupire Equation")
-# plt.plot(test_strikes, vol_est, lw=3, label="Dupire Equation")
+# plt.plot(test_strikes, z1, lw=3, label="Dupire Equation")
+plt.plot(test_strikes, vol_est, lw=3, label="Dupire Equation")
 plt.grid(True)
 plt.legend()
 plt.show()
