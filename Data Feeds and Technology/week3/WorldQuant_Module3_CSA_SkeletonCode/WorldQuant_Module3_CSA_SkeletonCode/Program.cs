ecbe7c5c909477ab2e754840646ea268b47920be
thquyen11 <thquyen11@hotmail.com>
2020-09-06 18:38:40 +0800

    add new course Data Feeds and Technology

---
+++ Data Feeds and Technology/week3/WorldQuant_Module3_CSA_SkeletonCode/WorldQuant_Module3_CSA_SkeletonCode/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WorldQuant_Module3_CSA_SkeletonCode
+{
+    class Program
+    {
+        static Excel.Workbook workbook;
+        static Excel.Application app;
+
+        static void Main(string[] args)
+        {
+            app = new Excel.Application();
+            app.Visible = true;
+            try
+            {
+                workbook = app.Workbooks.Open("property_pricing.xlsx", ReadOnly: false);
+            }
+            catch
+            {
+                SetUp();
+            }
+
+            var input = "";
+            while (input != "x")
+            {
+                PrintMenu();
+                input = Console.ReadLine();
+                try
+                {
+                    var option = int.Parse(input);
+                    switch (option)
+                    {
+                        case 1:
+                            try
+                            {
+                                Console.Write("Enter the size: ");
+                                var size = float.Parse(Console.ReadLine());
+                                Console.Write("Enter the suburb: ");
+                                var suburb = Console.ReadLine();
+                                Console.Write("Enter the city: ");
+                                var city = Console.ReadLine();
+                                Console.Write("Enter the market value: ");
+                                var value = float.Parse(Console.ReadLine());
+
+                                AddPropertyToWorksheet(size, suburb, city, value);
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Error: couldn't parse input");
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Mean price: " + CalculateMean());
+                            break;
+                        case 3:
+                            Console.WriteLine("Price variance: " + CalculateVariance());
+                            break;
+                        case 4:
+                            Console.WriteLine("Minimum price: " + CalculateMinimum());
+                            break;
+                        case 5:
+                            Console.WriteLine("Maximum price: " + CalculateMaximum());
+                            break;
+                        default:
+                            break;
+                    }
+                } catch { }
+            }
+
+            // save before exiting
+            workbook.Save();
+            workbook.Close();
+            app.Quit();
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Select an option (1, 2, 3, 4, 5) " +
+                              "or enter 'x' to quit...");
+            Console.WriteLine("1: Add Property");
+            Console.WriteLine("2: Calculate Mean");
+            Console.WriteLine("3: Calculate Variance");
+            Console.WriteLine("4: Calculate Minimum");
+            Console.WriteLine("5: Calculate Maximum");
+            Console.WriteLine();
+        }
+
+        static void SetUp()
+        {
+            // TODO: Implement this method
+        }
+
+        static void AddPropertyToWorksheet(float size, string suburb, string city, float value)
+        {
+            // TODO: Implement this method
+        }
+
+        static float CalculateMean()
+        {
+            // TODO: Implement this method
+            return 0.0f;
+        }
+
+        static float CalculateVariance()
+        {
+            // TODO: Implement this method
+            return 0.0f;
+        }
+
+        static float CalculateMinimum()
+        {
+            // TODO: Implement this method
+            return 0.0f;
+        }
+
+        static float CalculateMaximum()
+        {
+            // TODO: Implement this method
+            return 0.0f;
+        }
+    }
+}
