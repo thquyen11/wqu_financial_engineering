3156e4047dd96d767d6f22eb2fc9de6382a09176
thquyen11 <thquyen11@hotmail.com>
2022-07-13 00:49:15 +0800

    add class PerpetualBond

--- pricing_self_implement/Bond.py
+++ pricing_self_implement/Bond.py
@@ -3,73 +3,98 @@ from tkinter import Y
 
 
 class Bond:
-    def __init__(self, nominal, coupon, compounding, maturity, YTM, accrual=0):
+    def __init__(self, nominal=100, coupon_pct=0, compounding_freq=1, full_coupon_period=1, broken_period=0):
         self.nominal=nominal
-        self.coupon=coupon/100
-        self.compounding=compounding
-        self.mauturity=maturity
-        self.YTM=YTM/100
-        self.accrual=accrual
+        self.coupon_pct=coupon_pct
+        self.coupon=coupon_pct/100
+        self.compounding_freq=compounding_freq
+        self.full_coupon_period=full_coupon_period
+        # self.ytm=ytm_pct/100
+        self.broken_period=broken_period
+        self.accrual = (self.coupon_pct/self.compounding_freq)*(1-self.broken_period)/100
+
+    @classmethod
+    def from_zcb(cls, nominal=1, compounding_freq=1, full_coupon_period=1):
+        return cls(nominal=nominal, coupon_pct=0, compounding_freq=compounding_freq, full_coupon_period=full_coupon_period)
 
     def get_coupon_flow(self):
-        return [self.nominal*(self.coupon/self.compounding)]*int((self.compounding*(self.mauturity+self.accrual)))
+        return [self.nominal*(self.coupon/self.compounding_freq)]*int((self.compounding_freq*self.full_coupon_period))
 
     def get_capital_flow(self):
-        capital_flow = [0]*int((self.compounding*(self.mauturity+self.accrual)))
+        capital_flow = [0]*int((self.compounding_freq*self.full_coupon_period))
         capital_flow[-1]=self.nominal
         return capital_flow
 
-    def get_cashflow(self):
-        print("cash flow coupon:")
-        print(self.get_coupon_flow())
-        print("cash flow capital:")
-        print(self.get_capital_flow())
+    def dirty_price(self, ytm_pct, type=""):
+        return self.dirty_price_approx(ytm_pct) if type == "approx" or type == "approximate" else self.dirty_price_formula(ytm_pct)
 
-    def get_clean_price(self, type="formula"):
-        return self.get_clean_price_formula() if type == "formula" else self.get_clean_price_discounting_flows()
-
-    def get_clean_price_formula(self):
-        c = self.nominal*(self.coupon/self.compounding)
-        ytm = self.YTM
-        k = self.compounding
-        t = self.mauturity
+    def dirty_price_approx(self, ytm_pct):
+        c = self.nominal*(self.coupon_pct/self.compounding_freq)
+        ytm = ytm_pct/100
+        k = self.compounding_freq
+        t = self.full_coupon_period
         fv = self.nominal
 
-        coupon_flow_discount = (c/ytm)*(1-1/math.pow(1+ytm/k,k*t))
+        interest_flow_discount = (c/ytm)*(1-1/math.pow(1+ytm/k,k*t))
         capital_flow_discount = fv/math.pow(1+ytm/k,k*t)
-        return coupon_flow_discount + capital_flow_discount
 
-    def get_clean_price_discounting_flows(self):
-        c = self.nominal*(self.coupon/self.compounding)
-        ytm = self.YTM
-        k = self.compounding
-        t = self.mauturity
-        fv = self.nominal
-        accr = self.accrual
+        return interest_flow_discount + capital_flow_discount
 
-        count_payments = int(k*(t+accr))
-        clean_price = 0
-        for i in range(count_payments):
-            if i < count_payments - 1:
-                clean_price += c/math.pow(1+ytm/k,k*t)
-            else:
-                clean_price += c/math.pow(1+ytm/k,k*t) + fv/math.pow(1+ytm/k,k*t)
+    def dirty_price_formula(self,ytm_pct):
+        C = self.coupon_pct
+        ytm = ytm_pct/100
+        h = self.compounding_freq
+        n = self.full_coupon_period
+        N = 100
+        dt = self.broken_period
+        interest_flow_discount = 0
+        capital_flow_discount = 0
+        broken_period_flow_discount = 0
 
-        return clean_price
+        # zero coupon bond skip this step
+        if C != 0:
+            for i in range(n+1):
+                # if no broken period, skip coupon payment at t=0
+                if i == 0 and dt == 0:
+                    continue
+                else:
+                    interest_flow_discount += (C/h)/math.pow(1+ytm/h,i)
+            
+        capital_flow_discount = N/math.pow(1+ytm/h,n)
+        broken_period_flow_discount = 1/math.pow(1+ytm/h,dt)
+        
+        return broken_period_flow_discount*(interest_flow_discount + capital_flow_discount)
 
-    def get_accrual_amt(self):
-        return self.nominal*self.accrual*(self.coupon/self.compounding)
+    def clean_price(self, ytm_pct):
+        return self.dirty_price(ytm_pct) - self.accrual
 
-    def get_dirty_price(self):
-        return self.get_clean_price() + self.get_accrual_amt()
+    def YTM(self, price):
+        C = self.coupon_pct
+        N = 100
+        P = price
+        n = self.broken_period + self.full_coupon_period
+        return (C + (N-P)/n)/((N+P)/2)
+
+class PerpetualBond:
+    def __init__(self, nominal, coupon):
+        self.nominal = nominal
+        self.coupon = coupon
+
+    def price(interest_rate, growth_rate=0):
+        return self.nominal*self.coupon/(interest_rate+growth_rate)
 
 
-bond=Bond(nominal=150000,coupon=5,compounding=2,maturity=8.593,YTM=2,accrual=0.407)
-print(bond.get_coupon_flow())
-print(bond.get_capital_flow())
-print(bond.get_clean_price())
-print(bond.get_accrual_amt())
-print(bond.get_dirty_price())
+# zcb1=Bond.from_zcb(50000,2,3.5*2)
+# price1=zcb1.dirty_price(ytm_pct=2)*zcb1.nominal/100
+
+# bond1=Bond(nominal=100,coupon_pct=6,full_coupon_period=3,broken_period=0.5)
+# dirty_price1=bond1.dirty_price(ytm_pct=4)
+
+
+
+
+
+
 
 
 
